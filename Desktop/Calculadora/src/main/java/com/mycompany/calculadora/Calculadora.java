1403f5023fc5bb84404589505652cac71bdbe7ba
Erick Daniel <erickdanielmartinezmartinez123@gmail.com>
2024-09-01 21:57:39 -0600

    First Commit

---
+++ Desktop/Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import igu.Pantalla;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
